f230cebe212371b6146a2b69638320ff2663f4e1
Kirill <Kirill.IgnatovZ@yandex.ru>
2022-04-14 22:16:26 +0300

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask
 from data import db_session
 
@@ -13,3 +14,31 @@ def main():
 
 if __name__ == '__main__':
     main()
+=======
+from flask import Flask, render_template, redirect
+from data import db_session
+
+
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'nnwllknwthscd'
+db_session.global_init("db/content.db")
+
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('main_page.html')
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
+>>>>>>> 1023faf (first commit)
