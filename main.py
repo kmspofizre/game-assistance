1904108ac388c98185e7b0a3536ce41b3a9460ba
Kirill <Kirill.IgnatovZ@yandex.ru>
2022-04-23 18:48:22 +0300

    Начал добавлять обработку почты (требуется функция обработки регистрации, чтобы завершить)

--- main.py
+++ main.py
@@ -2,13 +2,26 @@ from flask import Flask, render_template, redirect
 from data import db_session
 from forms.user_registration import UserForm
 from forms.news_form import NewsForm
+from flask_login import LoginManager, login_required
+import csv
+from mail_sender import send_email
+from dotenv import load_dotenv
 
 
 app = Flask(__name__)
+load_dotenv()
 
 
 app.config['SECRET_KEY'] = 'nnwllknwthscd'
 db_session.global_init("db/content.db")
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+with open('mails.csv', encoding='utf-8') as mails_file:
+    mails = csv.DictReader(mails_file, delimiter=',', quotechar='"')
+    required = list(filter(lambda x: x['почтовый домен'] == 'mail.ru', mails))
+    print(required[0]['адрес для входа в почту'])
 
 
 @app.route('/')
@@ -39,6 +52,19 @@ def new():
 def add_news():
     news_form = NewsForm()
     return render_template('add_news.html', title='News', news_form=news_form)
+
+
+def post_form():
+    email = ''
+    if send_email(email, 'Email verification', 'link', []):  # отправляем на нее письмо
+        return f'Письмо отправлено успешно на адрес {email}'
+    return f'Во время отправки письма на адрес {email} произошла ошибка'
+
+
+@app.route('/check_email/<hashed_code>')
+@login_required
+def check_email(hashed_code):
+    pass
 
 
 def main():
